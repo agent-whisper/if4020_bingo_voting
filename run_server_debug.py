215db8db535ab305706e79f5ffa0585a2c66a082
fariz.tumbuan <fariz.tumbuan@prosa.ai>
2019-05-05 10:40:56 +0700

    updated and created server and client start debut script

--- run_server_debug.py
+++ run_server_debug.py
@@ -3,8 +3,6 @@ import os
 from src.server.bvm_server import server, config
 
 if __name__ == '__main__':
-    # Disables OAuthlib's HTTPs verification (Required for running locally).
-    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
     os.environ['FLASK_ENV']='development'
     ip = config['ip']
     port = config['port']
